64d5506450589142a30142e68e90edf42ee149d9
Martin Vasconcelo <martin.vasconcelo@gmail.com>
2022-06-03 21:25:39 -0300

    Conectamos Cucumber reports Online

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -12,7 +12,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "steps",
         monochrome = true,
-        tags = "@Marto"
+        tags = "@Marto",
+        publish = true
 )
 
 public class Runner {
